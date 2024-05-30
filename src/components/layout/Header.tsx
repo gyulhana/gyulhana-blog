c6aa89f0df8976235b864942225df927b6da4b14
gyulhana <matearz@korea.ac.kr>
2024-05-30 18:55:24 +0900

    feat: 헤더 UI 구현

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { FlexRowContainer, RowBlock, StyledLink, T1 } from '@/styles';
+import { HeaderContainer, Image } from '@/components/layout/Header.style';
+
+const profiles: { title: string; link: string }[] = [
+  {
+    title: 'resume',
+    link: 'https://www.rallit.com/resumes/417868@matearz/%EB%B0%95%EA%B7%9C%EB%9E%80',
+  },
+  { title: 'github', link: 'https://github.com/gyulhana' },
+  {
+    title: 'linkedIn',
+    link: 'https://www.linkedin.com/in/%EA%B7%9C%EB%9E%80-%E2%80%8D%EB%B0%95-7737b81aa/',
+  },
+];
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <FlexRowContainer>
+        <Image src="/public/images/gyulhana.jpeg" />
+        <RowBlock width={24} />
+        <T1>gyulhana.blog</T1>
+      </FlexRowContainer>
+
+      <FlexRowContainer>
+        {profiles.map(({ link, title }) => (
+          <React.Fragment key={title}>
+            <StyledLink href={link}>{title}</StyledLink>
+            {title !== 'linkedIn' && <RowBlock width={24} />}
+          </React.Fragment>
+        ))}
+      </FlexRowContainer>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
---
+++ src/styles/layout/Header.style.ts
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  height: 6rem;
+  padding: 20px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid black;
+`;
+
+export const Image = styled.img`
+  width: 3rem;
+  height: 3rem;
+  border-radius: 50%;
+`;
